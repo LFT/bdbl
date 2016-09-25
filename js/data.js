b7162ea5527e525f5e3b3a36aa93ae0cd0dcc610
Py <py.cladiere@telecom-bretagne.eu>
2016-09-25 14:31:53 +0200

    Linked more audio samples

--- js/data.js
+++ js/data.js
@@ -7,15 +7,68 @@ const quoteData = [{
         "src" : "dracula_drucker",
         "key" : "d",
         "name": "Drucker"
+    },
+    {
+        "src" : "dracula_kebab",
+        "key" : "k",
+        "name": "Kebab"
+    },
+    {
+        "src" : "dracula_passe",
+        "key" : "1",
+        "name": "Il s'en passe"
+    },
+    {
+        "src" : "dracula_norris",
+        "key" : "n",
+        "name": "Chuck N."
+    },
+    {
+        "src" : "germinal_lentier",
+        "key" : "l",
+        "name": "Le lait lentier"
+    },
+    {
+        "src" : "germinal_macarena",
+        "key" : "m",
+        "name": "Macarena"
+    },
+    {
+        "src" : "jeckyll_carglass",
+        "key" : "c",
+        "name": "Carglass"
+    },
+    {
+        "src" : "metamorphose_fajitas",
+        "key" : "f",
+        "name": "Fajitas"
+    },
+    {
+        "src" : "metamorphose_jv",
+        "key" : "j",
+        "name": "Jeux video.com"
     }
 ];
-
+// c, d, f, j, k, l, m, n, p, 1
+// ? p
 const videoData = [{	
 		"name" : "Cyrano",
 		"youtube" : ""
 	},
 	{
 		"name" : "Dracula",
+		"youtube" : ""
+	},
+	{
+		"name" : "Germinal",
+		"youtube" : ""
+	},
+	{
+		"name" : "Jeckyll",
+		"youtube" : ""
+	},
+	{
+		"name" : "Metamorphose",
 		"youtube" : ""
 	}
 ];
